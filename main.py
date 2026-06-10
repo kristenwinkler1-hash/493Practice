cb7b7639027775f4c5ef695f73fecddddcbdc685
Kristen Winkler <you@example.com>
2026-06-10 11:24:17 -0600

    created weatherdata class

---
+++ main.py
@@ -0,0 +1,19 @@
+class WeatherData:
+    def __init_(self):
+        self.latitude = 30.2672
+        self.longitude = -97.7431
+        self.month = 7
+        self.day = 13
+        self.year = 2025
+
+        self.avg_temperature = 0
+        self.min_temperature = 0
+        self.max_temperature = 0
+
+        self.avg_wind_speed = 0
+        self.min_wind_speed = 0
+        self.max_wind_speed = 0
+
+        self.sum_precipitation = 0
+        self.min_precipitation = 0
+        self.max_precipitation = 0
